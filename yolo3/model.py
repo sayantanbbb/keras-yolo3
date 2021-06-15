8f520ea43ed1c48b5ca2cd9f2c51835b5499c11b
sayantanbbb <71583332+sayantanbbb@users.noreply.github.com>
2021-06-15 21:33:18 +0530

    Update model.py

--- yolo3/model.py
+++ yolo3/model.py
@@ -4,12 +4,12 @@ from functools import wraps
 
 import numpy as np
 import tensorflow as tf
-from keras import backend as K
-from keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model
-from keras.regularizers import l2
+from tf.keras import backend as K
+from tf.keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
+from tf.keras.layers.advanced_activations import LeakyReLU
+from tf.keras.layers.normalization import BatchNormalization
+from tf.keras.models import Model
+from tf.keras.regularizers import l2
 
 from yolo3.utils import compose
 
